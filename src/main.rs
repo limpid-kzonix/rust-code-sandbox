72ea86614c8eb885c29b58dfaad934607c5c7e16
limpid-kzonix <balyszyn@gmail.com>
2019-03-04 22:36:25 +0200

    add custom trait for structure

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,13 @@ use iron::Response;
 
 fn main() {
     println!("Hello, world!");
-    let measurement = Measurement { temperature: 36.6, scale: "Celsius".to_owned() };
+    let measurement = Measurement { temperature: 36.6, scale: "Celsius" };
     println!("{}", measurement);
+
+    let addr: String = "127.0.0.1:8080".parse().unwrap_or("0.0.0.0:8080".to_owned());
+    println!("{} ", addr.as_str());
+
+    measurement.ext_fmt();
 
     fn hello_world(_: &mut Request) -> IronResult<Response> {
         Ok(Response::with((iron::status::Ok, "Hello World!")))
@@ -21,7 +26,7 @@ fn main() {
 
 struct Measurement {
     temperature: f32,
-    scale: String
+    scale: &'static str
 }
 
 impl Display for Measurement {
@@ -33,6 +38,17 @@ impl Display for Measurement {
         base_string.push_str(&(self.scale));
         _f.write_str(base_string.as_str())
     }
+}
+
+impl MyTrait for Measurement {
+    fn ext_fmt(&self) {
+        let string = format!("{} {}", self.temperature.to_string(), &self.scale);
+        println!("{}", string)
+    }
+}
+
+trait MyTrait {
+    fn ext_fmt(&self);
 }
 
 #[cfg(test)]
